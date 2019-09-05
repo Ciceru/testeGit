9be76cc3011062ce948ffb3c9a0d4382e6955ca9
OI402126 <OI402126@LAPCORPGO104812.oi.corp.net>
2019-09-04 22:14:04 -0300

    Primeiro Commit.

---
+++ br/com/principal/Teste.java
@@ -0,0 +1,12 @@
+package br.com.principal;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("O Bona e o cara!!!");
+
+	}
+
+}
